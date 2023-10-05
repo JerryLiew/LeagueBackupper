311967a7843a097770ae4d47274232eabb285c0c
jerry <1067082341@qq.com>
2023-10-06 01:03:28 +0800

    SDF

---
+++ Test/UtilityTest.cs
@@ -0,0 +1,17 @@
+﻿using LeagueBackupper.Utility;
+
+namespace Test;
+
+[TestClass]
+public class UtilityTest
+{
+
+    [TestMethod]
+    public void Test_SearchAndRenameClientParentFolderName()
+    {
+        
+        GameFolderUtility.SearchAndRenameClientParentFolderName("G:\\z2");
+    }
+    
+    
+}
